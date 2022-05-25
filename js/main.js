f0791d777203f3a20f532bd880fc3e73f5eab027
olesika <89180120+olesika@users.noreply.github.com>
2022-05-25 18:48:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+new SimpleLightbox({ elements: ".container-galerie a" });
+new SimpleLightbox({ elements: ".orar-imagini a" });
+
+AOS.init({
+    duration: 700,
+    offset: 100,
+    once: true
+});
+
+
+emailjs.init("M0s3vlJvF9dp0JhAv");
+function mobile() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className = "nav mobile";
+    } else {
+        x.className = "nav";
+    }
+}
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_15n654a", "template_69ic2xk", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+}
+
+
